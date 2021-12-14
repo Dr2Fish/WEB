5784f6731c05a54f93cf88e8c1c1ed0a38ba97ca
JesusEscbo193 <85661502+Dr2Fish@users.noreply.github.com>
2021-12-14 11:54:46 -0600

    index

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <title>Escuelita</title>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    />
+    <link rel="stylesheet" href="css/inicio.css" />
+    <link
+      href="https://file.myfontastic.com/QK2jUUaXUQQNzQLHfCpD63/icons.css"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <header class="header">
+      <div class="contenedor">
+        <a href="/"><h1 class="logo">MatePeques</h1></a>
+        <ul class="menu">
+          <li><a class="menu_link select">Inicio</a></li>
+          <li>
+            <a class="menu_link" href="registro.html"
+              >Registrarse <br />
+              </a
+            >
+          </li>
+          
+          <li>
+            <a class="menu_link" href="inicio_sesion.php"
+              >Iniciar sesi&oacute;n</a
+            >
+          </li>
+          <li>
+            <a class="menu_link" href="acerca_de.html">Acerca de nosotros</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <div class="banner">
+      <img
+        src="./img/2942361.jpg"
+        alt="Ni�os estudiando"
+        width="100%"
+        height="620px"
+        class="banner_img"
+      />
+      <div class="contenedor">
+        <h2>El mejor servicio web educativo gratuito a tu alcance</h2>
+        <p class="texto">
+          &Uacute;nete a nosotros y haz que sea m&aacute;s f&aacute;cil tu
+          aprendizaje
+        </p>
+      </div>
+    </div>
+    <footer class="footer">
+      <div class="social">
+        <a class="icon-facebook" href="https://www.facebook.com/"></a>
+        <a class="icon-twitter" href="https://www.twitter.com/"></a>
+        <a class="icon-gplus" href="https://www.google.com/"></a>
+        <a class="icon-vine" href="https://vine.co/"></a>
+      </div>
+      <p class="copy">&copy; MatePeques 2021 - Todos los derechos reservados</p>
+    </footer>
+  </body>
+</html>
